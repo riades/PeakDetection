0e99193ea803c5fa71c451c55e2d3049380e123c
Liza Ivanova <imst563@gmail.com>
2017-04-20 12:58:58 +0300

    deleted extra files

--- convolution/fftwconvolve.h
+++
@@ -1,8 +0,0 @@
-#ifndef FFTWCONVOLVE_H_INCLUDED
-#define FFTWCONVOLVE_H_INCLUDED
-
-#include "fftwwrappers.h"
-
-std::vector<double> fftwConvolve(const std::vector<double> &a, const std::vector<double> &b);
-
-#endif
